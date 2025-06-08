c6810e96e12599cc879513d0f4d20087d4e41d8e
gaoxy.franklin <gaoxy.franklin@gmail.com>
2025-06-08 16:44:19 +0800

    auth commit from franklin

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd ..
+cd ../src
+go build -o hostinfo main.go
+
 
 docker build -t registry.cn-hangzhou.aliyuncs.com/kubernetes_install/hostinfo:v0.0.1 .
 docker push registry.cn-hangzhou.aliyuncs.com/kubernetes_install/hostinfo:v0.0.1
